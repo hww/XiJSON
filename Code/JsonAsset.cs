256142a0fb3327e5678e8653bd9e0a31a0c95021
hww <valery.hww@gmail.com>
2022-10-23 19:22:23 +0300

    fix(code): Refactoring

--- Code/JsonAsset.cs
+++ Code/JsonAsset.cs
@@ -66,7 +66,7 @@ namespace XiJSON
         // Param:
         // archive -  The archive.
 
-        public bool Serialize(IArchive archive)
+        public virtual bool Serialize(IArchive archive)
         {
             var path = string.Empty;
             if (string.IsNullOrEmpty(relativePath))
